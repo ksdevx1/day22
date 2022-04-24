d738fea2067ed02207fc080fbbcb470ffa59b574
Kestas <k.sukevicius@gmail.com>
2022-04-24 08:31:56 +0300

    updates to use new classes and oher small fixes

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-from turtle import Screen
+import turtle
+from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
+from decoration import Decoration
+from speedometer import Speedometer
 import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
+screen.setup(width=800, height=700)
 screen.title("Pong")
+
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((370, 0))
+l_paddle = Paddle((-370, 0))
 ball = Ball()
 scoreboard = Scoreboard()
+decoration = Decoration()
+speedometer = Speedometer(ball.move_speed)
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "8")
-screen.onkey(r_paddle.go_down, "2")
+screen.onkeypress(r_paddle.go_up, "8")
+screen.onkeypress(r_paddle.go_down, "2")
 
-screen.onkey(l_paddle.go_up, "Up")
-screen.onkey(l_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "Up")
+screen.onkeypress(l_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
@@ -29,23 +35,27 @@ while game_is_on:
     ball.move()
 
     #Detect collision with wall
-    if ball.ycor() > 280 or ball.ycor() < -280:
+    if ball.ycor() > 275 or ball.ycor() < -275:
         #needs to bounce
         ball.bounce_y()
 
     #Detect collision with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        ball.bounce_x()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 350 or ball.distance(l_paddle) < 50 and ball.xcor() < -350:
+        ball.bounce_x() #ball move_speed will increase
+        speedometer.update_speed(ball.move_speed)
 
     #Detect R paddle misses
     if ball.xcor() > 380:
-        ball.reset_position()
+        ball.reset_position() #ball nove speed will be reset to 0.1
+        speedometer.update_speed(ball.move_speed)
         scoreboard.l_point()
 
     #Detect L paddle misses
     if ball.xcor() < -380:
-        ball.reset_position()
+        ball.reset_position() #ball nove speed will be reset to 0.1
+        speedometer.update_speed(ball.move_speed)
         scoreboard.r_point()
+
 
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
-from turtle import Turtle
+from turtle import *
+import turtle
 
 class Scoreboard(Turtle):
 
@@ -9,14 +10,22 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.lp_name = "Left Player"
+        self.rp_name = "Right Player"
         self.update_scoreboard()
+        self.get_names()
+        self.update_scoreboard()
+
+    def get_names(self):
+        self.lp_name = turtle.textinput("Left Player", "Enter your Name")
+        self.rp_name = turtle.textinput("Right Player", "Enter your Name")
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(-100, 200)
-        self.write(self.l_score, align="center", font=("Courier", 70, "normal"))
-        self.goto(100, 200)
-        self.write(self.r_score, align="center", font=("Courier", 70, "normal"))
+        self.goto(-200, 290)
+        self.write(self.lp_name + " " + str(self.l_score), align="center", font=("Courier", 20, "normal"))
+        self.goto(120, 290)
+        self.write(str(self.r_score) + " " + self.rp_name, align="center", font=("Courier", 20, "normal"))
 
     def l_point(self):
         self.l_score += 1
@@ -25,3 +34,4 @@ class Scoreboard(Turtle):
     def r_point(self):
         self.r_score += 1
         self.update_scoreboard()
+
